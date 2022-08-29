422190c95b0e905e9a2aedbc81fba9ed2475501c
fernandoarruda <fernandoarruda1384@gmail.com>
2022-08-28 21:34:19 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
     AlunoID int,
     Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+    Sobrenome varchar(100),
+    Endereco varchar(200),
+    Cidade varchar(80),
+    Host varchar(150)
 );
